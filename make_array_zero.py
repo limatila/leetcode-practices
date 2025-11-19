176a9101ad6dfc2d2514bef0cedb9fad12d6d3e2
Atila Lima <atilalimade@gmail.com>
2025-11-18 21:16:50 -0300

    95% feito, porém 'Time Limit Exceeded'
    Algoritmo com complexicidade O(n²), necessário refatorar pra passar...

--- make_array_zero.py
+++ make_array_zero.py
@@ -1,31 +1,46 @@
-# 3354. Make Array Elements Equal to Zero# 
+# 3354. Make Array Elements Equal to Zero#
+
 
 class Solution:
     going_right: bool = True
     count_selections: int = 0
 
     def change_direction(self, choice: bool = None):
-        if choice is not None: self.going_right = choice
-        self.going_right = not self.going_right
-    
+        if choice is not None:
+            self.going_right = choice
+        else:
+            self.going_right = not self.going_right
+
+    def go_next_position(self, current_index: int) -> int:
+        if self.going_right:
+            current_index += 1
+        else:
+            current_index -= 1
+
+        # print(current_index)
+        return current_index
+
     def choose_starting_point(self, zero_selection: int) -> int:
         if self.going_right:
             return zero_selection - 1
-        else: 
+        else:
             return zero_selection + 1
 
-    def will_go_out_of_bounds(self, index: int, nums: list[int]):
-        # print(f"analysing bounds on: {index}")
-        # return index < 0 or index > len(nums) - 1
-        
-        print(f"analysing bounds on: {index}, {'going right' if self.going_right else 'going left'}")
-        ultimo_index = (len(nums) - 1)
-        
+    def is_out_of_bounds(self, index: int, nums: list[int]) -> bool:
+        return index < 0 or index > len(nums) - 1
+
+    def will_go_out_of_bounds(self, index: int, nums: list[int]) -> bool:
+        print(
+            f"analysing bounds on: {index}, {'going right' if self.going_right else 'going left'}"
+        )
+
+        ultimo_index = len(nums) - 1
+
         if self.going_right:
-            #checa se uma adição ao valor sair dos index
+            # checa se uma adição ao valor sair dos index
             return (index + 1) > ultimo_index
         else:
-            #checa se uma remoção ao valor sair dos index
+            # checa se uma remoção ao valor sair dos index
             return (index - 1) < 0
 
     def countValidSelections(self, nums: list[int]) -> int:
@@ -43,34 +58,29 @@ class Solution:
                 current_index = self.choose_starting_point(possible_selection)
                 nums_copy = list(nums)
                 if self.will_go_out_of_bounds(current_index, nums_copy):
-                    print(f"skipado out of bounds: {'going right' if self.going_right else 'going left'} para {nums_copy}")
-                    continue
-                if (current_index < len(nums_copy) - 1 and
-                    nums_copy[current_index] == 0 and 
-                    current_index != -1
-                ):
-                    print(f"skipado 0 agregado: {'going right' if self.going_right else 'going left'} a partir de posição {current_index}")
+                    print(
+                        f"skipado out of bounds: {'going right' if self.going_right else 'going left'} para {nums_copy}"
+                    )
                     continue
 
-                print('starting:', nums_copy)
+                print("starting loop:", nums_copy)
                 print(current_index)
-                while( current_index >= 0 and current_index < len(nums_copy) ):
+                while True:
+                    current_index = self.go_next_position(current_index)
+                    
+                    if self.is_out_of_bounds(current_index, nums_copy):
+                        break;
+
                     if nums_copy[current_index] > 0:
                         nums_copy[current_index] -= 1
                         self.change_direction()
 
-                    if self.going_right: 
-                        current_index += 1
-                    else: 
-                        current_index -= 1
-                    print(current_index)
-
-                print('finished:', nums_copy)
+                print("finished loop:", nums_copy)
                 for number in nums_copy:
                     if number > 0:
-                        print('skipado incompleto')
+                        print("skipado incompleto")
                         increment_count_selections = False
-                        break;
+                        break
                     increment_count_selections = True
 
                 if increment_count_selections:
